43e9a6c4b88850f5dc2151c54d0a12a7a282d7de
Kuran V Kumar <kurankumar@gmail.com>
2019-10-01 20:59:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,18 +12,17 @@ finished_array
 end
 
 def reduce(source_array, start_point=nil)
+  if start_point 
+  actual_start_point = start_point
+  i = 0 
+else
+  actual_start_point = source_array[0]
+  i = 1 
+end
   
-  if start_point
-    redone_startPoint = start_point
-    i = 0
-  else
-    redone_startPoint = source_array[0]
-    i = 1
+  while i < source_array.length 
+    actual_start_point = yield(actual_start_point, source_array[i])
+    i += 1 
   end
-  
-  while i < source_array.length
-    redone_startPoint = yield(redone_startPoint, source_array[i])
-    i += 1
-  end
-  redone_startPoint
+  actual_start_point
 end
